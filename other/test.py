6bd8f5c32386dee8cc787b85f382f646b4f69f7d
owtf086 <owtf086@gmail.com>
2023-09-06 18:25:10 +0300

    Обновлен код проги

--- other/test.py
+++ other/test.py
@@ -1,65 +1,46 @@
 import numpy as np
-import pandas as pd
+from math import *
 
-h1, h2, h3 = 0.1, 0.05, 0.025  # step sizes
-y = 1.0  # initial condition
-
-# creating grids
-grid1 = np.arange(0, 1 + h1, h1)
-grid2 = np.arange(0, 1 + h2, h2)
-grid3 = np.arange(0, 1 + h3, h3)
+h1, h2, h3 = 0.1, 0.05, 0.025  # размеры шагов
 
 
-# defining functions
-def f(t, y):
+def f(t, y):  # исходная функция
     return 2 * t * y
 
 
-def f_solve(t):
-    return np.exp(t * t)
+def analytic(t):  # аналитическое значение
+    return exp(t ** 2)
 
 
-num_result, real_result = [], []
+a = 0
+b = 1
+y0 = 1.0  # начальное условие
 
-num_result.append(y)
+# Генерация массивов для сеток
+grid_h1 = np.arange(a, b + h1, h1)
+grid_h2 = np.arange(a, b + h2, h2)
+grid_h3 = np.arange(a, b + h3, h3)
 
-for i in grid1:
-    y_new = y + h1 * f(i, y)
-    num_result.append(y_new)
 
-    y_new_real = f_solve(i)
-    real_result.append(y_new_real)
-    y = y_new
+def method_1(y, h, t):
+    return y + h * f(t, y)
 
-y_new_real = f_solve(1)
-real_result.append(y_new_real)
 
-error = np.absolute(np.array(num_result) - np.array(real_result))
+def method_2(y, h, t):
+    return y + (h / 2) * (f(t, y) + f(t + h, y + h * f(t, y)))
 
-error_scientific = []
-for x in error:
-    error_scientific.append('{:.2e}'.format(x))
 
-df1 = pd.DataFrame([grid1, real_result, num_result, error_scientific]).T
-df1.columns = ['t', 'y_real', 'y_num (M1)', '|y_num-y_real| (M1)']
+def method_3(y, h, t):
+    return y + h * f(t + (h / 2), y + (h / 2) * f(t, y))
 
-print(df1)
-num_result = []
-y = 1.0
-num_result.append(y)
 
-for i in grid1:
-    y_new = y + (h1 / 2.0) * (f(i, y) + f(i + h1, y + h1 * f(i, y)))
-    num_result.append(y_new)
-    y = y_new
+def find_values(y, grid, h):
+    print("Сетка с разбиением", h)
+    for t in grid:
+        y_analytic = round(analytic(t), 14)
+        error = abs(y - y_analytic)
+        print(round(t, 7), round(y, 14), y_analytic, '%.2E' % error)
+        y = method_1(y, h, t)  # Номер метода
 
-error = np.absolute(np.array(num_result) - np.array(real_result))
 
-error_scientific = []
-for x in error:
-    error_scientific.append('{:.2e}'.format(x))
-
-df2 = pd.DataFrame([num_result, error_scientific]).T
-df2.columns = ['y_num (M2)', '|y_num-y_real| (M2)']
-
-print(df2)
+find_values(y0, grid_h1, h1)  # Вывод таблицы
